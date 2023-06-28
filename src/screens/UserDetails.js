fa338bf187b09a8c2a9b347be71b1e29e278f843
harsh <harshkhosla9945@gmail.com>
2023-06-28 16:40:19 +0530

    user details page completed

--- src/screens/UserDetails.js
+++ src/screens/UserDetails.js
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, StyleSheet, TouchableOpacity,TextInput,Dimensions,Text,Image } from 'react-native';
 import Background from '../components/Background';
-import Logo from '../components/Logo';
-import LogoLogin from '../components/LoginLogo';
 import Header from '../components/Header';
 import Button from '../components/Button';
-import TextInput from '../components/TextInput';
 import BackButton from '../components/BackButton';
-import { theme } from '../core/theme';
 import { useDispatch, useSelector } from 'react-redux';
-import { loginuser } from '../Redux/Action';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { UpdatName, loginuser } from '../Redux/Action';
 
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
 export default function UserDetails({ navigation }) {
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
@@ -20,83 +17,110 @@ export default function UserDetails({ navigation }) {
     name: '',
     password: '',
   });
+  console.log(data);
 
-  const saveData = async (data) => {
-    console.log(data?.password, 'enfhgevwfnbv');
-    console.log('wev,mn');
-    const { name, password } = data;
+  const Porduct_Key = useSelector(state => state?.userReducers?.setProductkey)
+  const id = useSelector(state => state?.userReducers?.Product?._id)
+  console.log(id);
 
-    // Perform input validation
-    if (name.trim() === '' || password.trim() === '') {
-      // Display an error or show a toast message indicating that the input is empty
-      return;
-    }
-
+  const onConnect = async () => {
     try {
-      const response = await fetch(
-        `http://192.168.4.1/?username=${name}&password=${password}&` +
-          JSON.stringify({
-            name,
-            password,
-          }),
-        {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json',
-          },
-        }
-      );
-      navigation.navigate('Home');
-      const result = await response.json();
-
-      // toast.success(result?.toast);
-      console.log(result, 'casdvas');
-      if (!result?.success) {
-        throw Error(result.error);
-        // navigation.navigate('Home');
+      const response = await fetch('http://192.168.4.1/');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      const responseData = await response.text();
+
+      // const parsedResponse = parse(responseData);
+      // const messageElement = parsedResponse.querySelector('h1');
+      // const message = messageElement?.text || '';
+      console.log(responseData);
     } catch (error) {
-      console.log(error, 'cvdsavs');
+      console.error('Error:', error);
     }
   };
 
+  const saveData = async (data) => {
+    
+    const { name, password } = data;
+
+    // Perform input validation
+    // if (name.trim() === '' || password.trim() === '') {
+    //   // Display an error or show a toast message indicating that the input is empty
+    //   return;
+    // }
+
+    try {
+      const response = await fetch(
+        `http://192.168.4.1/?username=${name}&password=${password}&`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+            password,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+     hit();
+      navigation.navigate('Home');
+
+      const result = await response.text();
+      console.log(result);
+      // Additional logic with the result
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const hit=()=>{
+    dispatch(UpdatName(Porduct_Key,id));
+  }
   return (
     <Background>
-      <BackButton goBack={navigation.goBack} />
-      <Logo />
-      <LogoLogin />
-      <Header>Create Account</Header>
-      <TextInput
-        label="Name"
-        returnKeyType="next"
-        name="name"
+      <BackButton/>
+      <Header style={styles.header}>Change Wi-Fi</Header>
+      <View>
+        {/* <Image
+          style={styles.image}
+        /> */}
+        <View >
+          <Text style={styles.inputtext}>
+            Change the connected Wi-Fi credentials to which the charger is connected:
+          </Text>
+        </View>
+      </View>
+      <View style={styles.overlayed}></View>
+      <View>
+        {/* Add circular rectangular TextInput components */}
+        <View style={styles.circularInputContainer1}>
+          <TextInput style={styles.circularInput} placeholder="SSID"  name="name"
         value={data.name}
-        onChangeText={(txt) => {
-          setData((prevData) => ({ ...prevData, name: txt }));
-        }}
-      />
-      <TextInput
-        label="Password"
-        returnKeyType="done"
-        name="password"
-        value={data.password}
-        onChangeText={(txt) => {
-          setData((prevData) => ({ ...prevData, password: txt }));
-        }}
-        secureTextEntry
-      />
-      <Button
-        mode="contained"
-        onPress={() => saveData(data)}
-        style={{ marginTop: 24 }}
-        disabled={loading}
-      >
-        {loading ? 'Loading...' : 'Sign Up'}
-      </Button>
-      <View style={styles.row}>
-        <Text>Already have an account? </Text>
-        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
-          <Text style={styles.link}>Login</Text>
+        onChangeText={(txt) => setData((prevData) => ({ ...prevData, name: txt }))}/>
+        </View>
+        <View style={styles.circularInputContainer2}>
+          <TextInput style={styles.circularInput} placeholder="Password"   name="password"
+        value={data.name}
+        onChangeText={(txt) => setData((prevData) => ({ ...prevData, password: txt }))}
+/>
+        </View>
+        <TouchableOpacity
+          style={styles.changeButton}
+          onPress={saveData}
+        >
+          <Text style={styles.changeButtonText}>Change</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.changeButton1}
+          onPress={() => navigation.navigate('Home')}
+        >
+          <Text style={styles.changeButtonText}>Skip</Text>
         </TouchableOpacity>
       </View>
     </Background>
@@ -104,12 +128,110 @@ export default function UserDetails({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  row: {
-    flexDirection: 'row',
-    marginTop: 4,
+  circularInputContainer1: {
+    borderWidth: 3,
+    borderColor: '#CCCCCC',
+    borderRadius: 20,
+    width: screenWidth * 0.8,
+    height: screenHeight * 0.07,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: screenHeight * -0.5,
+    top: screenHeight * 0.38,
+    
   },
-  link: {
+  circularInputContainer2: {
+    borderWidth: 3,
+    borderColor: '#CCCCCC',
+    borderRadius: 20,
+    width: screenWidth * 0.8,
+    height: screenHeight * 0.07,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: screenHeight * 0.06,
+    top: screenHeight * 0.37
+    
+  },
+  circularInput: {
+    width: '80%',
+    height: '80%',
+    fontSize: 16,
+    
+  },
+  changeButton: {
+    borderWidth: 2,
+    borderColor: '#118615',
+    borderRadius: 30,
+    width: screenWidth * 0.4,
+    height: screenHeight * 0.06,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: screenHeight * 0.13,
+    marginLeft: screenWidth * 0.18,
+    top: screenHeight * 0.33
+  },
+  changeButton1: {
+    borderWidth: 2,
+    borderColor: '#118615',
+    borderRadius: 30,
+    width: screenWidth * 0.4,
+    height: screenHeight * 0.06,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: screenHeight * 0.07,
+    marginLeft: screenWidth * 0.18,
+    top: screenHeight * 0.3
+  },
+  changeButtonText: {
+    fontSize: screenWidth * 0.04,
+    // fontWeight: 'bold',
+    color: 'black',
+  },
+  sample: {
+    fontSize: screenWidth * 0.05,
     fontWeight: 'bold',
-    color: theme.colors.primary,
+    paddingVertical: screenHeight * 0.012,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  inputtext: {
+    fontSize: screenWidth * 0.052,
+    marginTop: screenHeight * -0.8,
+    paddingTop: screenHeight * 0.7,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    top: screenHeight * -0.185,
+  },
+  header: {
+    fontSize: screenWidth * 0.05,
+    fontWeight: 'bold',
+    color: 'black',
+    marginTop: screenHeight * -0.13,
+    marginBottom: screenHeight * 0.06,
+    top: screenHeight * -0.34,
+  },
+  
+  
+  next: {
+    borderRadius: screenWidth * 0.03,
+    backgroundColor: 'green',
+    marginTop: screenHeight * 0,
+    width: screenWidth * 0.8,
+  },
+ 
+  
+  overlayed: {
+    position: 'absolute',
+    top: screenHeight * 0.08,
+    left: screenWidth * 0.05,
+    right: screenWidth * 0.05,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ 
+
+ 
 });
